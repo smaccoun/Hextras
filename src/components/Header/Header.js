28cf7e83f82df4885f7555502a77678904a87792
Steven MacCoun <theraccoun@gmail.com>
2016-06-15 10:14:01 -0700

    Changed basic titles and logo info to Hextras

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,7 +12,7 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Header.css';
 import Link from '../Link';
 import Navigation from '../Navigation';
-import logoUrl from './logo-small.png';
+import logoUrl from './logo-hextras.jpg';
 
 function Header() {
   return (
@@ -21,11 +21,11 @@ function Header() {
         <Navigation className={s.nav} />
         <Link className={s.brand} to="/">
           <img src={logoUrl} width="38" height="38" alt="React" />
-          <span className={s.brandTxt}>Your Company</span>
+          <span className={s.brandTxt}>Hextras</span>
         </Link>
         <div className={s.banner}>
-          <h1 className={s.bannerTitle}>React</h1>
-          <p className={s.bannerDesc}>Complex web apps made easy</p>
+          <h1 className={s.bannerTitle}>Hextras</h1>
+          <p className={s.bannerDesc}>Signing up extras made easy</p>
         </div>
       </div>
     </div>
